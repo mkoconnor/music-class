79c89471f6f52e0818ed0604e235363d36ee31d6
Michael O'Connor <mkoconnor@gmail.com>
2015-02-13 18:23:46 -0500

    expand slider more

--- beats/script.js
+++ beats/script.js
@@ -33,8 +33,8 @@ $(document).ready(function () {
     $("#secondFreq").html(otherFreq);
     
     $("#slider").slider({
-        min: defaultFreq * .5,
-        max: defaultFreq * 1.5,
+        min: defaultFreq * .8,
+        max: defaultFreq * 1.2,
         value: otherFreq,
         slide: function (event,ui) {
             var val = $(this).slider("value");
